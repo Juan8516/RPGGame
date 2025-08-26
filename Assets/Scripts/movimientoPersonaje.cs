faf9e891376f099a609682b9475373a88974c33d
Juan8516 <juan.ind85@gmail.com>
2025-08-26 18:50:41 -0500

    movimiento horizontal y lateral del personaje
    -se prueban movimientos
    
    -funcionamiento correcto de los valores de velocidad

--- Assets/Scripts/movimientoPersonaje.cs
+++ Assets/Scripts/movimientoPersonaje.cs
@@ -1,7 +1,19 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class movimientoPersonaje : MonoBehaviour
 {
+    [SerializeField] private float _velocidad;
+
+    private Rigidbody2D _rifgidbody2D;
+    private Vector2 _direccionMovimiento;
+    private Vector2 _input;
+
+    private void Awake()
+    {
+        _rifgidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +23,39 @@ public class movimientoPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _input = new Vector2(x:Input.GetAxisRaw("Horizontal"), y:Input.GetAxisRaw("Vertical"));
+
+        //Movimiento de personaje en X (horizontal)
+        if(_input.x > 0.1f)
+        {
+            _direccionMovimiento.x = 1f;
+        } 
+        else if (_input.x < 0f)
+        {
+            _direccionMovimiento.x = -1f;
+        }
+        else 
+        {
+            _direccionMovimiento.x = 0f;
+        }
+
+        //Movimiento de personaje en Y (vertical)
+        if (_input.y > 0.1f)
+        {
+            _direccionMovimiento.y = 1f;
+        }
+        else if (_input.y < 0f)
+        {
+            _direccionMovimiento.y = -1f;
+        }
+        else
+        {
+            _direccionMovimiento.y = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rifgidbody2D.MovePosition(_rifgidbody2D.position + _direccionMovimiento * _velocidad * Time.fixedDeltaTime);
     }
 }
