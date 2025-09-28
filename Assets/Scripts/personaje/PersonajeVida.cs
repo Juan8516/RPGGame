863d056f0634b831e4a356e69830cc55ad973d48
Juan8516 <juan.ind85@gmail.com>
2025-09-27 20:13:09 -0500

    Revivir personaje

--- Assets/Scripts/personaje/PersonajeVida.cs
+++ Assets/Scripts/personaje/PersonajeVida.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 public class PersonajeVida: VidaBase
 {
     public static Action EventoPersonajeDerrotado;
+
+    public bool Derrotado { get; private set; }
     public bool PuedeSerCurado => Salud < saludMax;
+
+    private BoxCollider2D _boxCollider2D;
+
+    private void Awake()
+    {
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
 
     override protected void Start()
     {
@@ -19,13 +28,18 @@ public class PersonajeVida: VidaBase
             RecibirDaño(10);
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.Y))
         {
             RestaurarSalud(10);
         }
     }
     public void RestaurarSalud(float cantidad)
     {
+        if(Derrotado)
+        {
+            return;
+        }
+
         if(PuedeSerCurado)
         {
             Salud += cantidad;
@@ -42,7 +56,17 @@ public class PersonajeVida: VidaBase
 
     protected override void PersonajeDerrotado()
     {
+        _boxCollider2D.enabled = false;
+        Derrotado = true;
         EventoPersonajeDerrotado?.Invoke();
+    }
+
+    public void RestaurarPersonaje()
+    {
+        _boxCollider2D.enabled = true;
+        Derrotado = false;
+        Salud = saludInicial;
+        ActualizarBarraVida(Salud, saludInicial);
     }
 
     override protected void ActualizarBarraVida(float vidaActual, float vidaMaxima)
